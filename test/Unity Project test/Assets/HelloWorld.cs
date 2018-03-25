c8c9f9c03de3f72a698a15d21248009c479416f1
jukaile <jukaile123@gmail.com>
2018-03-25 15:37:36 +0800

    helloworld

---
+++ test/Unity Project test/Assets/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        GUI.skin.label.fontSize = 100;
+        GUI.Label(new Rect(10, 10, Screen.width, Screen.height), "Hello World");
+    }
+}
